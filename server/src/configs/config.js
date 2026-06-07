1da06ac5bd6e0ac201e14d077119935af7dba7dd
Basu Saxena <basusaxena2003@gmail.com>
2026-06-08 01:57:16 +0530

    server: add client url in cors origins

--- server/src/configs/config.js
+++ server/src/configs/config.js
@@ -3,7 +3,7 @@ dotenv.config();
 
 export const config = {
   DB_URL: process.env.MONGODB_URL,
-  ALLOWED_ORIGINS: ["http://localhost:5173"],
+  ALLOWED_ORIGINS: ["http://localhost:5173" , process.env.CLIENT_URL],
   PORT: process.env.PORT,
   CLOUDINARY: {
     API_KEY: process.env.CLOUDINARY_API_KEY,
